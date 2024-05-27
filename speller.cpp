2eabd4ca91b43e31cfafa9b80605c165599db7d5
Enier González Ibarra <79170796+EnierGonzalezIbarra@users.noreply.github.com>
2024-05-27 00:36:34 +0000

    Finished

--- speller.cpp
+++ speller.cpp
@@ -1,93 +1,91 @@
 
 // Implements a dictionary's functionality
-#include <string>
-#include <functional>
-#include <iostream>
-#include <fstream>
 #include <array>
-// #include <algorithm>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
 
 // Default dictionary
 #define DICTIONARY "dictionaries/large"
 
-// Maximum length for a word
-#define LENGTH 45
-
-// Choose number of buckets in hash table
+// Number of buckets in hash table
 #define N 150000
 
 using namespace std;
 
 // Represents a node in a hash table
-class List_Node {
-    public:
+class List_Node
+{
+  public:
     string word;
-    List_Node* next;
+    List_Node *next;
 
-    List_Node(string _word): word(_word) {
-         next = nullptr;
+    List_Node(string _word) : word(_word)
+    {
+        next = nullptr;
     }
-    ~List_Node(void) {
-        if (next != nullptr) {
+    ~List_Node(void)
+    {
+        if (next != nullptr)
+        {
             delete next;
         }
     }
-    
+
     bool search(string _word_lower)
     {
         string lower_word = word;
         string lower_target = _word_lower;
 
-        std::transform(lower_word.begin(), lower_word.end(), lower_word.begin(), ::tolower);
-        
-        // cout << endl;
-        // cout << "Comparing: " << lower_word << " & " << lower_target << "\n";
-        // cout << "From: " << word << " & " << _word << "\n";
+        transform(lower_word.begin(), lower_word.end(), lower_word.begin(), ::tolower);
 
         if (lower_word == lower_target)
         {
-            // cout << "Matched. Now returning true\n";
             return true;
-        } else if (next == nullptr) {
-            // cout << "Any matched. Now returning false\n";
+        }
+        else if (next == nullptr)
+        {
             return false;
-        } else {
-            // cout << "Not matched. Now looking forward\n";
+        }
+        else
+        {
             return next->search(_word_lower);
         }
     }
 
-    void add(List_Node* new_node)
+    void add(List_Node *new_node)
     {
-        if (next == nullptr) {
+        if (next == nullptr)
+        {
             next = new_node;
-        } else {
-            new_node->next = next;    
+        }
+        else
+        {
+            new_node->next = next;
             next = new_node;
         }
     }
 };
 
-// Represents the hash table itself 
-class Hash_Table {
-    private:
-        array<List_Node*, N> table;
-        unsigned int word_count = 0;
-    public:
-
-
+// Represents the hash table itself
+class Hash_Table
+{
+  public:
+    array<List_Node *, N> table;
+    unsigned int word_count = 0;
     // Loads dictionary into memory, returning true if successful, else false
     Hash_Table(string dictionary)
     {
         table.fill(nullptr);
         // Open the dictionary file
-        ifstream dict_file(DICTIONARY, std::ios::in);
+        ifstream dict_file(DICTIONARY, ios::in);
 
-        if (!dict_file.is_open()) 
+        if (!dict_file.is_open())
         {
-            std::cout << "Unable to open dictionary file";
-        } else {
-            cout << "Dictionary file is open\n";
+            cout << "Unable to open dictionary file";
         }
         string word = "";
 
@@ -99,7 +97,6 @@ class Hash_Table {
             // Look for the right place for the word using the hash() function
             int place = hash_function(word);
 
-
             // Add the word to the hash table directly if place is empty
             if (table[place] == nullptr)
             {
@@ -108,13 +105,12 @@ class Hash_Table {
             // Solve collision if it is not empty
             else
             {
-                List_Node* new_node = new List_Node(word);
+                List_Node *new_node = new List_Node(word);
                 table[place]->add(new_node);
             }
             // At this point there is another word
             word_count++;
         }
-        cout << "Closing dictfile\n";
         dict_file.close();
     }
 
@@ -133,12 +129,15 @@ class Hash_Table {
     {
         // Use a recursive helper function to iterate the corresponding linked list and find a word
         string lower_target = word;
-        std::transform(lower_target.begin(), lower_target.end(), lower_target.begin(), ::tolower);
+        transform(lower_target.begin(), lower_target.end(), lower_target.begin(), ::tolower);
         int place = hash_function(lower_target);
-        // cout << place << "\n";
-        if (table[place] != nullptr) {
+
+        if (table[place] != nullptr)
+        {
             return table[place]->search(lower_target);
-        } else {
+        }
+        else
+        {
             return false;
         }
     }
@@ -159,103 +158,200 @@ class Hash_Table {
     }
 };
 
+class Miss
+{
+  public:
+    unsigned int id;
+    string misspelled_word;
+    unsigned int line_number;
+    array<string, 3> suggestions;
 
-int main(int argc, char* argv[])
+    Miss(unsigned int _id, string _misspelled_word, unsigned int _line_number) : id(_id), line_number(_line_number)
+    {
+        misspelled_word = _misspelled_word;
+        transform(misspelled_word.begin(), misspelled_word.end(), misspelled_word.begin(), ::tolower);
+    }
+
+    void fill_suggestions(const Hash_Table& table)
+    {
+        int comparisons = 0;
+        int best_score = 0;
+        int second_to_best_score = 0;
+        int third_to_best_score = 0;
+
+        string best_suggestion = "";
+        string second_to_best_suggestion = "";
+        string third_to_best_suggestion = "";
+
+        for (int i = 0; i < N; i++)
+        {
+            if (table.table[i] != nullptr)
+            {
+
+                List_Node *list = table.table[i];
+                while (list != nullptr)
+                {
+                    string word = list->word;
+                    if (word.length()) {
+                        int score = get_similarity(word);
+                        comparisons++;
+
+                        if (score > best_score)
+                        {
+                            best_score = score;
+                            best_suggestion = word;
+                        }
+                        else if (score > second_to_best_score)
+                        {
+                            second_to_best_score = score;
+                            second_to_best_suggestion = word;
+                        }
+                        else if (score > third_to_best_score)
+                        {
+                            third_to_best_score = score;
+                            third_to_best_suggestion = word;
+                        }
+
+                        if (list->next != nullptr)
+                        {
+                            list = list->next;
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        suggestions[0] = best_suggestion;
+        suggestions[1] = second_to_best_suggestion;
+        suggestions[2] = third_to_best_suggestion;
+    }
+
+    int get_similarity(const string _target_word)
+    {
+
+        int score = 0;
+
+        int lenght_difference = misspelled_word.length() - _target_word.length();
+
+        switch (lenght_difference)
+        {
+        case 0:
+            score += 10;
+            break;
+        case 1:
+        case -1:
+            score += 5;
+            break;
+        case 2:
+        case -2:
+            score += 2;
+            break;
+        default:
+            break;
+        }
+
+        int smaller_length =
+            (misspelled_word.length() < _target_word.length()) ? misspelled_word.length() : _target_word.length();
+
+        for (int i = 0; i < smaller_length; i++)
+        {
+            if (misspelled_word[i] == _target_word[i])
+            {
+                score += 4;
+            }
+        }
+
+        return score;
+    }
+};
+
+int main(int argc, char *argv[])
 {
     // Check for correct number of args
     if (argc != 2 && argc != 3)
     {
-        std::cout << "Usage: ./speller [DICTIONARY] text\n";
+        cout << "Usage: ./speller [DICTIONARY] text\n";
         return 1;
     }
 
-    // Determine dictionary to use if provided 
+    // Determine dictionary to use if provided
     const string dictionary = (argc == 3) ? argv[1] : DICTIONARY;
 
     Hash_Table table(dictionary);
 
     // Try to open text
     string text = (argc == 3) ? argv[2] : argv[1];
-    ifstream file (text, std::ios::in);
+    ifstream file(text, ios::in);
     if (!file.is_open())
     {
-        std::cout << "Could not open text" <<  text << "\n";
+        cout << "Could not open text" << text << "\n";
         return 1;
-    } else {
-        std:: cout << "Opened text correctly\n";
+    }
+    else
+    {
+        cout << "Opened text correctly\n";
     }
 
     // Prepare to report misspellings
-    std::cout << "\nMISSPELLED WORDS\n\n";
+    cout << "\nMISSPELLED WORDS\n\n";
 
     // Prepare to spell-check
+    vector<Miss> misses;
     int index = 0, misspellings = 0, words = 0;
-    string word;
+    string word = "";
 
     // Spell-check each word in text
     char c;
+    string line;
+    unsigned int line_number = 0;
     while (file.peek() != EOF)
     {
-        file.get(c);
-        // Allow only alphabetical characters and apostrophes
-        if (isalpha(c) || (c == '\'' && index > 0))
+        getline(file, line, '\n');
+        istringstream line_stream(line);
+        line_number++;
+
+        while (line_stream.peek() != EOF)
         {
-            // Append character to word
-            string string_from_c(1, c);
-            word.append(string_from_c);
-            index++;
+            getline(line_stream, word, ' ');
 
-            // Ignore alphabetical strings too long to be words
-            if (index > LENGTH)
+            bool ignore_word = false;
+
+            if (word.length() == 1)
             {
-                // Consume remainder of alphabetical string
-                while (file.get(c)) 
-                {
-                    if (!isalpha(c))
-                    {
-                        break;
-                    }
-                }
-
-                // Prepare for new word
-                index = 0;
+                ignore_word = true;
             }
-        }
 
-        // Ignore words with numbers (like MS Word can)
-        else if (isdigit(c))
-        {
-            // Consume remainder of alphanumeric string
-            while (file.get(c)) 
+            for (int i = 0; i < word.length(); i++)
             {
-                if (!isalnum(c))
+                char c = word[i];
+                if (isdigit(c))
                 {
+                    ignore_word = true;
+                    break;
+                }
+                if (!isalpha(c))
+                {
+                    ignore_word = true;
                     break;
                 }
             }
-            // Prepare for new word
-            index = 0;
-        }
 
-        // We must have found a whole word
-        else if (index > 0)
-        {
-            // Update counter
             words++;
-
-            // Check word's spelling
-            bool misspelled = !table.check(word);
-
-            // Print word if misspelled
-            if (misspelled)
+            if (!ignore_word && word.length() != 0)
             {
-                std::cout << word << "\n";
-                misspellings++;
-            }
+                bool misspelled = !table.check(word);
 
-            // Prepare for next word
-            index = 0;
-            word.clear();
+                // Print word if misspelled
+                if (misspelled)
+                {
+                    misspellings++;
+                    misses.push_back(Miss(misspellings, word, line_number));
+                    cout << setw(5) << misspellings << ": " << setw(20) << word << setw(15)
+                              << "Line: " << line_number << "\n";
+                }
+            }
         }
     }
 
@@ -263,38 +359,60 @@ int main(int argc, char* argv[])
     if (file.bad())
     {
         file.close();
-        std::cout << "Error reading " << text << "\n";
+        cout << "Error reading " << text << "\n";
         return 1;
     }
 
-    //
-    // // Determine dictionary's size
-    // unsigned int n = table.size();
-    //
-    //
-    // // Unload dictionary
-    // bool unloaded = table.unload();
-    //
-    // // Abort if dictionary not unloaded
-    // if (!unloaded)
-    // {
-    //     std::cout << "Could not unload " << dictionary << "\n";
-    //     return 1;
-    // }
-    //
-    //
-    // // Report results
-    // std::cout << "\nWORDS MISSPELLED:     " << misspellings << "\n";
-    // std::cout << "WORDS IN DICTIONARY:  " << n << "\n";
-    // std::cout << "WORDS IN TEXT:        " << words << "\n";
-    //
-    // Close text
+    // Determine dictionary's size
+    unsigned int n = table.size();
+
+    // Report results
+    cout << "\nWORDS MISSPELLED:     " << misspellings << "\n";
+    cout << "WORDS IN DICTIONARY:  " << n << "\n";
+    cout << "WORDS IN TEXT:        " << words << "\n";
+    cout << "LINES IN TEXT:        " << line_number << "\n";
+
+    while (true)
+    {
+        cout << endl;
+        cout << "Select a misspelling ID from above (First column) to get suggestions, type any letter to exit: ";
+
+            string id_candidate = "";
+            int id = -1;
+
+            getline(cin, id_candidate);
+
+            try
+            {
+                id = stoi(id_candidate) - 1;
+            }
+            catch (const invalid_argument &e)
+            {
+                cout << "Closing the program now" << endl;
+                break;    
+            }
+            if (id < 0)
+            {
+                cout << "Invalid input. ID must be positive. Please try again\n";
+                continue;
+            }
+
+            if (id < misses.size())
+            {
+                misses[id].fill_suggestions(table);
+            }
+            else
+            {
+                cout << "Invalid input. ID must be smaller than number of misspellings";
+                continue;
+            }
+
+            cout << endl;
+            cout << "Suggestions: " << endl;
+            cout << "1. " << misses[id].suggestions[0] << endl;
+            cout << "2. " << misses[id].suggestions[1] << endl;
+            cout << "3. " << misses[id].suggestions[2] << endl;
+        }
     file.close();
-    // Return Success
     return 0;
 }
-
-
-
-
-
